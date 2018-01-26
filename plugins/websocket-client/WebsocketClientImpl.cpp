db7038fef5231c57863f677e6c18e7336db30c5f
Shumpei Urabe <urabe@hack.ne.jp>
2018-01-26 21:35:05 +0900

    wip

--- plugins/websocket-client/WebsocketClientImpl.cpp
+++ plugins/websocket-client/WebsocketClientImpl.cpp
@@ -8,10 +8,10 @@ OBS_DECLARE_MODULE()
 
 bool obs_module_load(void)
 {
-	return true;
+    return true;
 }
 
-WEBSOCKETCLIENT_API WebsocketClient* createWebsocketClient(void)
+WEBSOCKETCLIENT_API WebsocketClient *createWebsocketClient(void)
 {
     return new WebsocketClientImpl();
 }
@@ -22,7 +22,7 @@ WebsocketClientImpl::WebsocketClientImpl()
     client.set_access_channels(websocketpp::log::alevel::all);
     client.clear_access_channels(websocketpp::log::alevel::frame_payload);
     client.set_error_channels(websocketpp::log::elevel::all);
-    
+
     // Initialize ASIO
     client.init_asio();
 }
@@ -33,43 +33,45 @@ WebsocketClientImpl::~WebsocketClientImpl()
     disconnect(false);
 }
 
-bool WebsocketClientImpl::connect(std::string url, long long room, std::string username, std::string token, WebsocketClient::Listener* listener)
+bool WebsocketClientImpl::connect(std::string url, long long room, std::string username, std::string token, WebsocketClient::Listener *listener)
 {
     websocketpp::lib::error_code ec;
-    
+
     //reset loggin flag
     logged = false;
     try
     {
         // Register our message handler
-        client.set_message_handler( [=](websocketpp::connection_hdl con, message_ptr frame) {
-            const char* x = frame->get_payload().c_str();
+        client.set_message_handler([=](websocketpp::connection_hdl con, message_ptr frame) {
+            const char *x = frame->get_payload().c_str();
             //get response
             auto msg = json::parse(frame->get_payload());
-            std::cout << x << std::endl << std::endl << std::endl ;
-            
-            
+            std::cout << x << std::endl
+                      << std::endl
+                      << std::endl;
+
             //Check if it is an event
-            if (msg.find("janus") == msg.end())
+            if (msg.find("sora") == msg.end())
                 //Ignore
                 return;
-            
-            std::string id = msg["janus"];
-            
-            if (msg.find("ack") != msg.end()){
+
+            std::string id = msg["sora"];
+
+            if (msg.find("ack") != msg.end())
+            {
                 // Ignore
                 return;
             }
-            
+
             //Get response
-            std::string response = msg["janus"];
+            std::string response = msg["sora"];
             if (msg.find("jsep") != msg.end())
             {
                 std::string sdp = msg["jsep"]["sdp"];
                 listener->onOpened(sdp);
                 return;
             }
-            
+
             //Check type
             if (id.compare("success") == 0)
             {
@@ -77,14 +79,15 @@ bool WebsocketClientImpl::connect(std::string url, long long room, std::string u
                 if (msg.find("transaction") == msg.end())
                     //Ignore
                     return;
-                
-                if (msg.find("data") == msg.end()){
+
+                if (msg.find("data") == msg.end())
+                {
                     //Ignore
                     return;
                 }
                 //Get the Data session
                 auto data = msg["data"];
-                
+
                 //Server is sending response twice, ingore second one
                 if (!logged)
                 {
@@ -93,59 +96,51 @@ bool WebsocketClientImpl::connect(std::string url, long long room, std::string u
                     //Launch logged event
                     //create handle command
                     json attachPlugin = {
-                        {"janus", "attach"	},
-                        {"transaction", std::to_string(rand())	},
+                        {"sora", "attach"},
+                        {"transaction", std::to_string(rand())},
                         {"session_id", session_id},
-                        {"plugin", "janus.plugin.videoroom"},
+                        {"plugin", "sora.plugin.videoroom"},
                     };
-                    
+
                     connection->send(attachPlugin.dump());
                     //Logged
                     logged = true;
-
-                }else {
+                }
+                else
+                {
                     handle_id = data["id"];
-                    
+
                     json joinRoom = {
-                        {"janus", "message"	},
+                        {"sora", "message"},
                         {"transaction", std::to_string(rand())},
                         {"session_id", session_id},
-                        {"handle_id",  handle_id},
-                        { "body" ,
-                            {
-                                { "room"	, room },
-                                {"display"  , "OBS"},
-                                {"ptype"    , "publisher"},
-                                {"request"  , "join"}
-                            }
-                        }
-                    };
+                        {"handle_id", handle_id},
+                        {"body",
+                         {{"room", room},
+                          {"display", "OBS"},
+                          {"ptype", "publisher"},
+                          {"request", "join"}}}};
                     connection->send(joinRoom.dump());
                     listener->onLogged(session_id);
                 }
             }
         });
-        
-        
+
         //When we are open
-        client.set_open_handler([=](websocketpp::connection_hdl con){
+        client.set_open_handler([=](websocketpp::connection_hdl con) {
             //Launch event
             listener->onConnected();
             //Login command
             json login = {
-                {"janus", "create"},
-                {"transaction",std::to_string(rand()) },
-                { "payload",
-                    {
-                        { "username", username},
-                        { "token", token},
-                        { "room", room}
-                    }
-                }
-            };
+                {"sora", "create"},
+                {"transaction", std::to_string(rand())},
+                {"payload",
+                 {{"username", username},
+                  {"token", token},
+                  {"room", room}}}};
             //Serialize and send
             connection->send(login.dump());
-            
+
         });
         //Set close hanlder
         client.set_close_handler([=](...) {
@@ -161,44 +156,45 @@ bool WebsocketClientImpl::connect(std::string url, long long room, std::string u
         client.set_tls_init_handler([&](websocketpp::connection_hdl connection) {
             //Create context
             auto ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::tlsv1);
-            
-            try {
+
+            try
+            {
                 ctx->set_options(asio::ssl::context::default_workarounds |
                                  asio::ssl::context::no_sslv2 |
                                  asio::ssl::context::single_dh_use);
             }
-            catch (std::exception& e) {
+            catch (std::exception &e)
+            {
                 std::cout << e.what() << std::endl;
             }
             return ctx;
         });
         //Get connection
         connection = client.get_connection(url, ec);
-        
-        if (ec) {
+
+        if (ec)
+        {
             std::cout << "could not create connection because: " << ec.message() << std::endl;
             return 0;
         }
-        connection->add_subprotocol("janus-protocol");
-        
+        connection->add_subprotocol("sora-protocol");
+
         // Note that connect here only requests a connection. No network messages are
         // exchanged until the event loop starts running in the next line.
         client.connect(connection);
-        
+
         //Async
         thread = std::thread([&]() {
             // Start the ASIO io_service run loop
             // this will cause a single connection to be made to the server. c.run()
             // will exit when this connection is closed.
-            
+
             client.run();
         });
-        
-        
     }
-    
-    
-    catch (websocketpp::exception const & e) {
+
+    catch (websocketpp::exception const &e)
+    {
         std::cout << e.what() << std::endl;
         return false;
     }
@@ -212,31 +208,29 @@ bool WebsocketClientImpl::open(const std::string &sdp)
     {
         //Login command
         json open = {
-            { "janus"           , "message"	},
-            { "session_id"		, session_id},
-            { "handle_id"		, handle_id},
-            { "transaction"		, std::to_string(rand()) },
-            { "body" ,
-                {
-                    { "request"	,"configure" },
-                    {"muted", false},
-                    {"video", true},
-                    {"audio", true},
-                }
-            },
-            { "jsep" ,
-                {
-                    { "type"  ,"offer" },
-                    {"sdp"    ,sdp},
-                    {"trickle",true},
-                }
-            }
-        };
+            {"sora", "message"},
+            {"session_id", session_id},
+            {"handle_id", handle_id},
+            {"transaction", std::to_string(rand())},
+            {"body",
+             {
+                 {"request", "configure"},
+                 {"muted", false},
+                 {"video", true},
+                 {"audio", true},
+             }},
+            {"jsep",
+             {
+                 {"type", "offer"},
+                 {"sdp", sdp},
+                 {"trickle", true},
+             }}};
         //Serialize and send
         if (connection->send(open.dump()))
             return false;
     }
-    catch (websocketpp::exception const & e) {
+    catch (websocketpp::exception const &e)
+    {
         std::cout << e.what() << std::endl;
         return false;
     }
@@ -252,44 +246,38 @@ bool WebsocketClientImpl::trickle(const std::string &mid, int index, const std::
         {
             //Login command
             json trickle = {
-                { "janus"       , "trickle" },
-                { "handle_id"   , handle_id },
-                { "session_id"	, session_id },
-                { "transaction"	, "trickle" + std::to_string(rand()) },
-                { "candidate" ,
-                    {
-                        { "sdpMid"			, mid		},
-                        { "sdpMLineIndex"	, index		} ,
-                        { "candidate"		, candidate	}
-                    }
-                }
-            };
+                {"sora", "trickle"},
+                {"handle_id", handle_id},
+                {"session_id", session_id},
+                {"transaction", "trickle" + std::to_string(rand())},
+                {"candidate",
+                 {{"sdpMid", mid},
+                  {"sdpMLineIndex", index},
+                  {"candidate", candidate}}}};
             //Serialize and send
             if (connection->send(trickle.dump()))
                 return false;
-            
+
             //OK
             return true;
         }
         else
         {
             json trickle = {
-                { "janus"       , "trickle" },
-                { "handle_id"   , handle_id },
-                { "session_id"	, session_id },
-                { "transaction"	, "trickle" + std::to_string(rand()) },
-                { "candidate"   ,
-                    {
-                        { "completed", true },
-                    }
-                }
-            };
+                {"sora", "trickle"},
+                {"handle_id", handle_id},
+                {"session_id", session_id},
+                {"transaction", "trickle" + std::to_string(rand())},
+                {"candidate",
+                 {
+                     {"completed", true},
+                 }}};
             if (connection->send(trickle.dump()))
                 return false;
-            
         }
     }
-    catch (websocketpp::exception const & e) {
+    catch (websocketpp::exception const &e)
+    {
         std::cout << e.what() << std::endl;
         return false;
     }
@@ -299,36 +287,36 @@ bool WebsocketClientImpl::trickle(const std::string &mid, int index, const std::
 
 bool WebsocketClientImpl::disconnect(bool wait)
 {
-    
+
     try
     {
         // Stop keepAlive
 
-        
         //Stop client
         client.close(connection, websocketpp::close::status::normal, std::string("disconnect"));
         client.stop();
-        
+
         //Don't wait for connection close
         if (thread.joinable())
         {
             //If sswe have to wait
-            if (wait) {
+            if (wait)
+            {
                 thread.join();
             }
-            else {
+            else
+            {
                 //Remov hanlders
-                client.set_open_handler([](...){});
-                client.set_close_handler([](...){});
+                client.set_open_handler([](...) {});
+                client.set_close_handler([](...) {});
                 client.set_fail_handler([](...) {});
                 //Detach trhead
                 thread.detach();
-                
             }
         }
-        
     }
-    catch (websocketpp::exception const & e) {
+    catch (websocketpp::exception const &e)
+    {
         std::cout << e.what() << std::endl;
         return false;
     }
